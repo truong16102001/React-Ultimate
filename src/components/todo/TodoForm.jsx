fc1c79956419b42e04827723688aeb3bb9ee757c
TruongND <truongx62001@gmail.com>
2025-05-26 10:50:51 +0700

    apply DOM events

--- src/components/todo/TodoForm.jsx
+++ src/components/todo/TodoForm.jsx
@@ -1,9 +1,20 @@
 const TodoForm = ({addNew}) => {
-    addNew("Hello world")
+    //addNew("Hello world")
+    const handleClick = () => {
+        console.log("handleClick")
+    }
+    const handleChange = (message) => {
+        console.log("handleChange: " + message)
+    }
+
     return (
         <div className="todo-form">
-            <input type="text" />
-            <button>Add</button>
+            <input type="text" 
+                onChange={(event) => {handleChange(event.target.value)}}/>
+            <button
+                style={{cursor:"pointer"}}
+                onClick={handleClick}
+            >Add</button>
         </div>
     )
 }
