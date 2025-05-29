864130335b9d19b830181fda0654e9071563ec87
TruongND <truongx62001@gmail.com>
2025-05-29 15:22:58 +0700

    Router Integration

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import LoginPage from "./pages/Login.jsx";
+import RegisterPage from "./pages/Register.jsx";
+import UserPage from "./pages/Users.jsx";
+import "./styles/global.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  { path: "/", element: <App /> },
+  { path: "/auth/login", element: <LoginPage /> },
+  { path: "/auth/register", element: <RegisterPage /> },
+  { path: "/manage/users", element: <UserPage /> },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
---
+++ src/pages/Users.jsx
@@ -0,0 +1,8 @@
+const UserPage = () => {
+  return (
+    <>
+      <div>User management page</div>
+    </>
+  );
+};
+export default UserPage;
